8e5120224ba6f8595e14c6131466be6f7b2da3ce
freeman <freelogin@qq.com>
2019-06-23 06:15:13 +0800

    添加项目文件。

---
+++ WnStock/WnStock.h
@@ -0,0 +1,33 @@
+﻿
+// WnStock.h: WnStock 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CWnStockApp:
+// 有关此类的实现，请参阅 WnStock.cpp
+//
+
+class CWnStockApp : public CWinApp
+{
+public:
+	CWnStockApp() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWnStockApp theApp;
---
+++ WnStock/WnStockView.h
@@ -0,0 +1,49 @@
+﻿
+// WnStockView.h: CWnStockView 类的接口
+//
+
+#pragma once
+
+
+class CWnStockView : public CView
+{
+protected: // 仅从序列化创建
+	CWnStockView() noexcept;
+	DECLARE_DYNCREATE(CWnStockView)
+
+// 特性
+public:
+	CWnStockDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CWnStockView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // WnStockView.cpp 中的调试版本
+inline CWnStockDoc* CWnStockView::GetDocument() const
+   { return reinterpret_cast<CWnStockDoc*>(m_pDocument); }
+#endif
+
