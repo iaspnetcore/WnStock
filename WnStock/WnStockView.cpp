9774fb1f79767d5411bc92f57ab7c7e8d15943bd
freeman <freelogin@qq.com>
2019-09-26 20:48:42 +0800

    all

--- WnStock/WnStockView.cpp
+++ WnStock/WnStockView.cpp
@@ -51,12 +51,14 @@ BOOL CWnStockView::PreCreateWindow(CREATESTRUCT& cs)
 
 // CWnStockView 绘图
 
-void CWnStockView::OnDraw(CDC* /*pDC*/)
+void CWnStockView::OnDraw(CDC* pDC)
 {
 	CWnStockDoc* pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
+
+	DrowMyStockUI(pDC);
 
 	// TODO: 在此处为本机数据添加绘制代码
 }
@@ -103,3 +105,88 @@ CWnStockDoc* CWnStockView::GetDocument() const // 非调试版本是内联的
 
 
 // CWnStockView 消息处理程序
+
+void CWnStockView::DrowMyStockUI(CDC* pDC)
+{
+	CWnStockDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	//设置背景颜色
+	CRect rect;
+	GetClientRect(&rect);
+	CSize totalSize = 32;
+	if (rect.bottom <= totalSize.cy)
+	{
+		rect.bottom = totalSize.cy;
+	}
+
+	//pDC->SetBkMode(TRANSPARENT);   //透明
+	////pDC->SetBkMode(OPAQUE);
+	//pDC->SetBkColor(RGB(0, 0, 0));
+	//pDC->SelectStockObject(BLACK_BRUSH);
+	//pDC->Rectangle(&rect);
+
+	//添加修改选中股的底色
+	CRect rect1;
+	rect1.left = rect.left;
+	rect1.right = rect.right;
+	rect1.top = rect.top + 30 + 20 * (m_selectLine - 1);
+	rect1.bottom = rect.top + 30 + 20 * m_selectLine;
+	pDC->SetBkColor(RGB(255, 255, 0));
+	pDC->SelectStockObject(GRAY_BRUSH);
+	pDC->Rectangle(&rect1);
+
+	GetClientRect(&rect);
+
+	//获得数据
+	CString strLine;
+	//m_totalNum = pDoc->vMystock.size();
+	m_totalNum = 32;
+
+	m_width = 90;
+	m_height = 20;
+
+	//CString strLine;
+
+	//pDC->SetTextColor(RGB(200, 200, 200));
+
+	/*strLine.Format("编号");
+	pDC->TextOut(m_width / 8, 0 + m_posY, strLine);
+	strLine.Format("代码");
+	pDC->TextOut(m_width / 8 + m_width, 0 + m_posY, strLine);
+	strLine.Format("名称");
+	pDC->TextOut(m_width / 8 + 2 * m_width, 0 + m_posY, strLine);
+	strLine.Format("涨幅");
+	
+
+	pDC->TextOut(m_width / 8 + 3 * m_width, 0 + m_posY, strLine);
+	strLine.Format("现价");
+	pDC->TextOut(m_width / 8 + 4 * m_width, 0 + m_posY, strLine);
+	strLine.Format("涨跌");
+	pDC->TextOut(m_width / 8 + 5 * m_width, 0 + m_posY, strLine);
+	strLine.Format("开盘价");
+	pDC->TextOut(m_width / 8 + 6 * m_width, 0 + m_posY, strLine);
+	strLine.Format("最高价");
+	pDC->TextOut(m_width / 8 + 7 * m_width, 0 + m_posY, strLine);
+	strLine.Format("最低价");
+	pDC->TextOut(m_width / 8 + 8 * m_width, 0 + m_posY, strLine);
+	strLine.Format("成交量");
+	pDC->TextOut(m_width / 8 + 9 * m_width, 0 + m_posY, strLine);*/
+	strLine.Format("成交额");
+	pDC->TextOut(m_width / 8 + 10 * m_width, 0 + m_posY, strLine);
+	
+	CClientDC dc(this);
+	
+	// dc.TextOut(0, 0, _T("这是一个MFC程序!"));
+	//dc.TextOutW(8 + 10 , 0 , _T("成交额"));
+
+	
+
+
+
+
+	
+	return;
+}
--- WnStock/WnStockView.h
+++ WnStock/WnStockView.h
@@ -40,6 +40,27 @@ protected:
 // 生成的消息映射函数
 protected:
 	DECLARE_MESSAGE_MAP()
+
+	/*******************自定义变量*****************************/
+public:
+	//CRect rect;
+	bool		bScroll;		//是否显示滚动条
+	CString		m_stockCode;	//当前股票代码
+	CString		m_stockName;	//当前股票名称
+
+	//自选股UI所用变量
+	int			m_drawStatus;	//0:股票展示，1:画分时线，2:画K线
+	int			m_selectLine;   //表示自选股中当前选中的行
+	int			m_mouseLine;	//表示自选股中鼠标所在的行
+	int			m_totalNum;		//My Stock的数量
+	int			m_width;		//自选股区每一行显示数据的增量
+	int			m_height;		//自选股区每一列显示数据的增量
+	int			m_posY;			//滚动条下滑距离顶部高度，固定自选界面首行使用
+
+	/******************自定义函数******************************/
+protected:
+	//自选股UI所用函数
+	void		DrowMyStockUI(CDC* pDc);			//画自选股部分
 };
 
 #ifndef _DEBUG  // WnStockView.cpp 中的调试版本
