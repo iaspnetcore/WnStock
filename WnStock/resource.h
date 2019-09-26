b90b9d67dddb9b1bd3e3e3a9da3d40a425da700c
freeman <freelogin@qq.com>
2019-09-26 21:06:04 +0800

    按钮

--- WnStock/resource.h
+++ WnStock/resource.h
@@ -7,14 +7,17 @@
 #define IDR_MAINFRAME                   128
 #define IDR_WnStockTYPE                 130
 #define IDD_DIALOG1                     310
+#define IDD_ttt                         311
+#define IDD_DIALOG2                     312
+#define IDC_COMBO1                      1001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        315
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           310
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           312
 #endif
 #endif
